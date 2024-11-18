67f238f20e5ae9d522bfb7c2b1c433948182d470
Abduraxim <toxirmalk@gmail.com>
2024-11-18 14:55:23 +0500

    Loyiha boshlandi

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,17 @@
-console.log('Salom Dunyo')
-console.log('Salom JS')
+// console.log('Salom Dunyo')
+// console.log('Salom JS')
 
-console.log('For loop and while loop')
+// console.log('For loop and while loop')
+
+export const registercontroller = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+}
+export const logincontroller = async (req, res) => {
+    try {
+    } catch (error) {
+        res.status(400).send(error.message)
+    }
+}
