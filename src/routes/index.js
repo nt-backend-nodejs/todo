1585d74f1a363cec9c6ccf5912d35b102c8dcce5
Xorazm92 <rayadunia2021@gmail.com>
2024-11-18 14:29:32 +0500

    routes

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import {
+    registerController,
+    loginController,
+    verifyController,
+} from '../controller/index.js'
+
+export const authRouter = express.Router()
+
+authRouter.post('/register', registerController)
+authRouter.post('/login', loginController)
+authRouter.post('/verifyToken', verifyController)
