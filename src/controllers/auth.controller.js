d9f9bcbf4f31dea777f363c12be8647d5eba6b67
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2024-11-19 16:25:56 +0500

    be good person

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,7 +2,8 @@ console.log('hi')
 console.log('hello')
 console.log("I'm Abdulaziz")
 
+console.log('YO toshkent nima gap....')
 
-console.log("YO toshkent nima gap....");
+console.log('Salom JS')
 
-console.log("Salom JS")
+console.log("!I'm happy ...")
